b1f5c4efc7ead59ba2d01f737eb39300458789b4
TheLastKey420 <44385454+TheLastKey420@users.noreply.github.com>
2018-11-13 04:03:03 +0800

    Add files via upload

---
+++ jr.sh
@@ -0,0 +1,130 @@
+clear
+blue='\e[1;34m'
+green='\e[1;32m'
+purple='\e[1;35m'
+cyan='\e[1;36m'
+red='\e[1;31m'
+white='\e[1;37m'
+yellow='\e[1;33m'
+echo -e $green
+echo "Chemod_zrd404...................................";
+sleep 0.1;
+clear
+echo -e $green
+echo '21D4N404................';
+sleep 0.1;
+clear
+echo -e $red
+echo 'Chemod_zrd404..................';
+sleep 0.1;
+clear
+echo -e $green
+echo "21D4N404..............................";
+sleep 0.1;
+clear
+echo -e $red
+echo 'Chemod_zrd404................................';
+sleep 0.1;
+clear
+echo -e $green
+echo '21D4N404.............................';
+sleep 0.1;
+clear
+echo -e $red
+echo "Chemod_zrd404.....................";
+sleep 0.1;
+echo -e $green
+echo "Chemod_zrd404.................";
+sleep 0.1;
+clear
+echo -e $green
+echo '21D4N404................';
+sleep 0.1;
+clear
+echo -e $red
+echo 'Chemod_zrd404..................';
+sleep 0.1;
+clear
+echo -e $green
+echo "21D4N404..............................";
+sleep 0.1;
+clear
+echo -e $red
+echo 'Chemod_zrd404................................';
+sleep 0.1;
+clear
+echo -e $green
+echo '21D4N404.............................';
+sleep 0.1;
+clear
+echo -e $red
+echo "Chemod_zrd404.....................";
+sleep 0.1;
+clear
+echo -e $green
+echo "21D4N404.....................";
+sleep 0.1;
+echo -e $green
+echo "Chemod_zrd404.............................";
+sleep 0.1;
+clear
+echo -e $green
+echo '21D4N404................';
+sleep 0.1;
+clear
+echo -e $red
+echo 'Chemod_zrd404..................';
+sleep 0.1;
+clear
+echo -e $green
+echo "21D4N404..............................";
+sleep 0.1;
+clear
+echo -e $red
+echo 'Chemod_zrd404................................';
+sleep 0.1;
+clear
+echo -e $green
+echo '21D4N404.............................';
+sleep 0.1;
+clear
+echo -e $red
+echo "Chemod_zrd404.....................";
+sleep 0.1;
+echo -e $green
+echo "Chemod_zrd404............................";
+sleep 0.1;
+clear
+echo -e $green
+echo '21D4N404................';
+sleep 0.1;
+clear
+echo -e $red
+echo 'Chemod_zrd404..................';
+sleep 0.1;
+clear
+echo -e $green
+echo "21D4N404..............................";
+sleep 0.1;
+clear
+echo -e $red
+echo 'Chemod_zrd404................................';
+sleep 0.1;
+clear
+echo -e $green
+echo '21D4N404.............................';
+sleep 0.1;
+clear
+echo -e $red
+echo "Chemod_zrd404.....................";
+sleep 0.1;
+clear
+apt install figlet -y
+apt install curl -y
+clear
+apt install bash -y
+pkg install php -y
+clear
+clear
+echo
+echo
